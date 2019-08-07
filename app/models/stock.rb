147f2007914d88c52a7eee97f0cc6e21a17f0dd3
Rodrigo Oliveira <rmomogi@gmail.com>
2019-08-07 14:12:41 +0000

    Fix model stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,5 +8,5 @@ class Stock < ApplicationRecord
 
   scope :quantity_exists, -> { where('quantity > 0').order('products.name') }
   scope :by_category, ->(category) { where('products.category_id = ?', category) }
-  scope :by_search, ->(name) { where('products.name like ?', '%' << name << '%') }
+  scope :by_search, ->(name) { where('products.name like ?', "%#{name}%") }
 end
