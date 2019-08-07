850787c38e3e882c7b3a858c3a06980e69fbe0ab
Rodrigo Oliveira <rmomogi@gmail.com>
2019-08-07 03:25:37 +0000

    Fix spec DashboardHelper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,15 +2,13 @@
 
 module DashboardHelper
   def category_tree(category)
-    # binding.pry
-    html = ''
+    html = ""
     return html unless category
 
     html << link_to('Todas categorias', authenticated_root_path) << ' -> ' unless category.ancestors?
-
     category.path.each do |child|
-      html << link_to(child.name, authenticated_root_path(category_id: child.id))
-      html << ' -> ' unless child == category
+      html += link_to(child.name, authenticated_root_path(category_id: child.id))
+      html += ' -> ' unless child == category
     end
     html.html_safe
   end
